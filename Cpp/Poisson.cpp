44311795fd88798b23982129a74dc22f0ec01ef9
Samadrita Karmakar <sam@sam-laptop>
2019-10-18 12:29:27 +0200

    Poisson update

--- Cpp/Poisson.cpp
+++ Cpp/Poisson.cpp
@@ -88,7 +88,7 @@ public:
 
 int main(int argc, char *argv[])
 {
-    if(argc==1 || argc>3)
+    if(argc<3 || argc>3)
     {
         std::cout<<"Usage: ./Poisson <.msh Filename> <Dimension>\n";
         return 0;
